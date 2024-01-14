3c3b2d6d9f5da78c94532cc1588ebd4d0d29608f
AntonKhPI <152258209+AntonKhPI@users.noreply.github.com>
2024-01-14 21:16:15 +0200

    Update Reverse text.cpp

--- Reverse text.cpp
+++ Reverse text.cpp
@@ -2,9 +2,9 @@
 
 int main() {
     std::string a;
+    char b;
     std::cout << "Please write a text: ";
     std::getline(std::cin, a);
-    char b;
     for(int i = 0; i <= (a.size()-1)/2; i++){
         b = a[i];
         a[i] = a[a.size() - 1 - i];
